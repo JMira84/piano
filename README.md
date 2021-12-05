e24a43b51cd7f4ef0b61efe203d5e81190485c5d
João Mira <joao.mira01@gmail.com>
2021-12-05 12:09:26 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Brief
+
+In this project, we're creating a keyboard. I love this project because at first glance it seems easy, but there are some more complicated steps involved.
+
+You can use as many (or as few) tools, libraries, and frameworks as you'd like. If you're trying to learn something new, this would be a great way to push yourself.
+
+**Users should be able to:**
+
+- See the keyboard centered on the page
+- Whenever a user hovers over a specific key it will change colors
+  - White keys will change to yellow `#ffd200`
+  - Black keys will change to pink `#f40082`
+
---
+++ js/script.js
@@ -0,0 +1,51 @@
+const numberOfWhiteKeys = 13;
+
+function renderWhiteKeys () {
+    const pianoWrapper = document.querySelector('.piano-wrapper');
+    for (let i = 0; i < numberOfWhiteKeys; i++) {
+        const createdKeyWrappers = pianoWrapper.appendChild(document.createElement('div'));
+        const whiteKey = createdKeyWrappers.appendChild(document.createElement('div'));
+        
+        createdKeyWrappers.classList.add('keys-wrapper');
+        whiteKey.classList.add('white-key', 'white-key' + [i]);
+        whiteKey.setAttribute('id', 'key-' + [i + 1]);
+    }
+}
+
+function renderBlackKeys () {
+    const keyWrappers = document.querySelectorAll('.keys-wrapper');
+    for (let i = 0; i < keyWrappers.length; i++) {
+        if (i === 2 || i === 6 || i === 9 || i === 13) {
+            continue;
+        }
+        const blackKey = keyWrappers[i].appendChild(document.createElement('div'));
+
+        blackKey.classList.add('black-key', 'black-key' + [i]);
+        if (i >= 0 && i <= 1) {
+            blackKey.setAttribute('id', 'key-' + [i + 14]);
+        } else if (i >= 3 && i <= 5) {
+            blackKey.setAttribute('id', 'key-' + [i + 14 - 1]);
+        } else if (i >= 7 && i <= 8) {
+            blackKey.setAttribute('id', 'key-' + [i + 14 - 2]);
+        } else {
+            blackKey.setAttribute('id', 'key-' + [i + 14 - 3]);
+        }
+    }
+}
+
+function playSounds () {
+    document.addEventListener('click', e => {
+        const audio = new Audio();
+        const src = `./assets/sounds/${e.target.id}.mp3`;
+        audio.src = src;
+        audio.currentTime = 0;
+        audio.play();
+        return audio;
+    });
+}
+
+window.onload = function() {
+    renderWhiteKeys();
+    renderBlackKeys();
+    playSounds();
+}
